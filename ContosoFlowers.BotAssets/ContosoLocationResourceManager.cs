45be589855d1a683ec3cddba0def472af2019acf
Petr Dobroucky <petr.dobroucky@icord.cz>
2018-01-04 17:39:00 +0100

    Textual changes

--- ContosoFlowers.BotAssets/ContosoLocationResourceManager.cs
+++ ContosoFlowers.BotAssets/ContosoLocationResourceManager.cs
@@ -9,19 +9,19 @@
     {
         public override string ConfirmationAsk => Resources.Location_ConfirmationAsk;
         public override string SelectLocation => "Teď budu potřebovat Vaši fakturační adresu.";
-        public override string InvalidLocationResponse => "Hmm, to nemůže být správně. Nepřepsal si se? Zkus to prosím znovu.";
-        public override string LocationNotFound => "Tuto adresu jsem nenašel :( Zkus to prosím znovu";
-        public override string MultipleResultsFound => "Našel jsem více míst, vyber prosím to správné.";
-        public override string SingleResultFound => "Je to tahle adresa?";
-        public override string DialogStartBranchAsk => "Napiš svoji adresu";
+        public override string InvalidLocationResponse => "Hmm, to nemůže být správně. Nepřepsal jste se? Zkuste to prosím znovu.";
+        public override string LocationNotFound => "Tuto adresu jsem nenašel :( Zkuste to prosím znovu";
+        public override string MultipleResultsFound => "Našel jsem více míst, vyberte prosím to správné.";
+        public override string SingleResultFound => "Je to tato adresa?";
+        public override string DialogStartBranchAsk => "Napište svou adresu";
         public override string HelpCommand => "pomoc";
-        public override string HelpMessage => "Není ti pomoci";
+        public override string HelpMessage => "Není Vám pomoci";
         public override string Locality => "Kraj";
-        public override string PostalCode => "PSC";
+        public override string PostalCode => "PSČ";
         public override string Region => "Oblast";
         public override string ResetCommand => "znovu";
         public override string ResetPrompt => "Dobře začínáme znovu";
         public override string StreetAddress => "Adresa";
-        public override string TitleSuffix => "Napiš svoji adresu";
+        public override string TitleSuffix => "Napište svou adresu";
     }
 }
